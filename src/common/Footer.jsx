4e52786c4158877936c394494562a6ae59209149
Tomas Wajnerman <frutyram@gmail.com>
2023-08-13 12:50:12 -0300

    Perfeccionamos el footer agregando el logo

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -4,21 +4,38 @@ import {
   AiOutlineTwitter,
   AiOutlineGithub,
 } from "react-icons/ai";
-import "/styles/footer.css"
+import "/styles/footer.css";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="bg-dark text-light py-4 text-center">
-      <div>
-        <p>&copy; Todos los derechos reservados</p>
-      </div>
-      <div>
-        <Link to={'https://www.instagram.com/tomi_waj/'}><AiOutlineInstagram className="igLogo logo"></AiOutlineInstagram></Link>
-        <Link to={'https://www.facebook.com/tomas.wajnerman'}><AiOutlineFacebook className="fcLogo logo"></AiOutlineFacebook></Link>
-        <Link to={'https://twitter.com/TomasitoWaj'}><AiOutlineTwitter className="twLogo logo"></AiOutlineTwitter></Link>
-        <Link to={'https://github.com/Grefing'}><AiOutlineGithub className="ghLogo logo"></AiOutlineGithub></Link>
-      </div>
+      <div className="footerContainer">
+        <div className="align-self-center">
+          <Link to={'/'}><img src="/src/assets/logo.png" className="logo" alt="" /></Link>
+        </div>
+
+    
+          <div className="align-self-center">
+            <p className="nameFooter">&copy;Tomas Wajnerman</p>
+          </div>
+
+          <div className="align-self-center">
+            <Link to={"https://www.instagram.com/tomi_waj/"}>
+              <AiOutlineInstagram className="igLogo logo"></AiOutlineInstagram>
+            </Link>
+            <Link to={"https://www.facebook.com/tomas.wajnerman"}>
+              <AiOutlineFacebook className="fcLogo logo"></AiOutlineFacebook>
+            </Link>
+            <Link to={"https://twitter.com/TomasitoWaj"}>
+              <AiOutlineTwitter className="twLogo logo"></AiOutlineTwitter>
+            </Link>
+            <Link to={"https://github.com/Grefing"}>
+              <AiOutlineGithub className="ghLogo logo"></AiOutlineGithub>
+            </Link>
+          </div>
+        </div>
+     
     </footer>
   );
 };
